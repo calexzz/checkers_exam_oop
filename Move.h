a36ab4bd27f7686057856511783e0cd5c4e650f9
alejandrogurtsov <alex.1046296@gmail.com>
2026-05-29 18:11:37 +0800

    fix: added new fields to work withBoard's class methods

--- Move.h
+++ Move.h
@@ -1,12 +1,15 @@
 #ifndef CHECKERS_MOVE_H
 #define CHECKERS_MOVE_H
 
+class Figure;
 class Cell;
 
 struct Move {
     Cell* src;
     Cell* dst;
     Cell* captured;
+    bool wasPromotion;
+    Figure* capturedFigure;
 };
 
 #endif //CHECKERS_MOVE_H
